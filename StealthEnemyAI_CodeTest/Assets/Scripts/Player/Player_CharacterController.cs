245da40c0c62db5ea8f2fda7ee796e65cb15d639
Jared Ramey <jared.ramey@students.aie.edu.au>
2018-03-15 13:55:49 -0700

    Player is almost ready to move. Input handler has been created

---
+++ StealthEnemyAI_CodeTest/Assets/Scripts/Player/Player_CharacterController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player_CharacterController : MonoBehaviour
+{
+    #region Variables_Private
+    private Rigidbody playerBody;
+    [SerializeField]
+    private float moveForce = 0.0f;
+    [HideInInspector]
+    private bool isCrouched = false;
+    #endregion Variables_Private
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Initialize variables
+        playerBody = gameObject.GetComponent<Rigidbody>();
+
+        //Input listeners
+        Player_InputHandler.Instance.OnMoveForward.AddListener(Handle_OnMoveForward);
+        Player_InputHandler.Instance.OnMoveLeft.AddListener(Handle_OnMoveLeft);
+        Player_InputHandler.Instance.OnMoveBackward.AddListener(Handle_OnMoveBackward);
+        Player_InputHandler.Instance.OnMoveRight.AddListener(Handle_OnMoveRight);
+        Player_InputHandler.Instance.OnCrouch.AddListener(Handle_OnCrouch);
+        Player_InputHandler.Instance.OnDropCigarette.AddListener(Handle_OnDropCigarette);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region Listener_Handles
+    private void Handle_OnMoveForward()
+    {
+        Debug.Log("Moving forward");
+        playerBody.AddForce(((Vector3.forward) * moveForce) * Player_InputHandler.Instance.vertical);
+    }
+    private void Handle_OnMoveLeft()
+    {
+
+    }
+    private void Handle_OnMoveBackward()
+    {
+
+    }
+    private void Handle_OnMoveRight()
+    {
+
+    }
+    private void Handle_OnCrouch()
+    {
+
+    }
+    private void Handle_OnDropCigarette()
+    {
+
+    }
+    #endregion Listener_Handles
+}
---
+++ StealthEnemyAI_CodeTest/Assets/Scripts/Player/Player_InputHandler.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player_InputHandler : MonoBehaviour
+{
+
+    #region UnityEvent_InitArea
+    [HideInInspector]
+    public UnityEvent OnMoveForward = new UnityEvent();
+    [HideInInspector]
+    public UnityEvent OnMoveLeft = new UnityEvent();
+    [HideInInspector]
+    public UnityEvent OnMoveBackward = new UnityEvent();
+    [HideInInspector]
+    public UnityEvent OnMoveRight = new UnityEvent();
+    [HideInInspector]
+    public UnityEvent OnCrouch = new UnityEvent();
+    [HideInInspector]
+    public UnityEvent OnDropCigarette = new UnityEvent();
+    #endregion UnityEvent_InitArea
+
+    #region Variable_InitArea
+    [SerializeField]
+    [HideInInspector]
+    public float horizontal = 0.0f, vertical = 0.0f;
+
+    private static Player_InputHandler instance = null;
+
+    public static Player_InputHandler Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = (Player_InputHandler)FindObjectOfType(typeof(Player_InputHandler));
+                if (instance == null)
+                {
+                    instance = (new GameObject("Player_InputManager")).AddComponent<Player_InputHandler>(); ;
+                }
+            }
+            return instance;
+        }
+    }
+    #endregion Variable_InitArea
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        #region Player_Movement
+        //Move forward
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Calling move forward");
+            OnMoveForward.Invoke();
+        }
+        //Move Left
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            OnMoveLeft.Invoke();
+        }
+        //Move Backward
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            OnMoveBackward.Invoke();
+        }
+        //Move Right
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            OnMoveRight.Invoke();
+        }
+        //crouch
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            OnCrouch.Invoke();
+        }
+        //Drop a cigarette
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            OnDropCigarette.Invoke();
+        }
+        #endregion Player_Movement
+    }
+}
